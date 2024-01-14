1127b00281f47af1f6d3465ec7402eb9f54c18c0
Denis <deni2012_95@mail.ru>
2024-01-14 16:43:19 +0300

    guesNumber v1.2

---
+++ угадай число/guessNumberApp.js
@@ -0,0 +1,11 @@
+
+function guessNum(num) {
+    while(true) {
+        const secretNumber = Math.ceil(Math.random() * 10);
+        if (num === secretNumber) {
+            console.log('Вы угадали!'); 
+            return secretNumber;}
+            else console.log('Секретное число:', secretNumber, 'Попробуйте еще!') 
+            return secretNumber;
+        }
+    };
